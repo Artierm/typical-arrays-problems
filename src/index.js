f70a8526ea2332de7ea4c3a143b62bead754a386
Artierm <artyom.rus.ct100@mail.ru>
2020-02-20 11:30:55 +0300

    feat: do it

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if(array==undefined || array.length==0)
+  {
+    return 0;
+  }
+     let min=array[0];
+      for(let index=0;index<array.length;index++){
+  
+          if (array[index]<min)
+          {
+              min=array[index];   
+          }
+          
+      };
+     return  min;
+  }
+  
+
 
 exports.max = function max (array) {
-  return 0;
-}
+  if(array==undefined || array.length==0 )
+  { 
+    return 0;
+  }
+     let max=array[0];
+      for(let index=0; index<array.length; index++){
+  
+          if (array[index]>max)
+          {
+              max=array[index];   
+          }
+          
+      };
+     return  max;
+  }
+
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array==undefined || array.length==0)
+  {
+    return 0;
+  }
+   let del = array.length;
+   let sum = 0;
+    for (let index=0; index<array.length; index++)
+    {
+    sum+=array[index];
+    }
+    return sum/del;
 }
+
